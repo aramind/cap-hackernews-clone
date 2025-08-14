749491ff4f8d855624a4b4c6258fdd77de3da82a
aramind <88814622+aramind@users.noreply.github.com>
2025-08-14 18:41:52 +0800

    feat: adding crud resolvers and schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const { ApolloServer } = require("apollo-server");
 
 let links = [
@@ -8,33 +10,52 @@ let links = [
   },
 ];
 
-const typeDefs = `type Query{
-    info: String!
-    feed: [Link!]!    
-    }
-    
-    type Link {
-    id: ID!
-    description: String!
-    url: String!
-    }
-    `;
-
 const resolvers = {
   Query: {
     info: () => `This is the API of Hackernews clone`,
     feed: () => links,
+    // fetch single link by ID
+    link: (parent, args) => {
+      return links.find((link) => link.id === args.id) || null;
+    },
   },
 
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+
+    // update
+    updateLink: (parent, args) => {
+      const link = links.find((link) => link.id === args.id);
+      if (!link) return null;
+
+      if (args.url !== undefined) link.url = args.url;
+      if (args.description !== undefined) link.description = args.description;
+
+      return link;
+    },
+
+    deleteLink: (parent, args) => {
+      const index = links.findIndex((link) => link.id === args.id);
+      if (index === -1) return null;
+
+      const [deletedLink] = links.splice(index, 1);
+      return deletedLink;
+    },
   },
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
 });
 
