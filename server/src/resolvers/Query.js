9527243518f9298bb71e904e55bbb03e67d0869d
aramind <88814622+aramind@users.noreply.github.com>
2025-08-16 12:06:49 +0800

    feat: add filtering

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,5 +1,13 @@
 function feed(parent, args, context) {
-  return context.prisma.link.findMany();
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {};
+  return context.prisma.link.findMany({ where });
 }
 
 module.exports = {
