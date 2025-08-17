9d5bd1e8cfbebdd92e26025d5e9d47f5e84622e6
aramind <88814622+aramind@users.noreply.github.com>
2025-08-17 23:18:21 +0800

    feat: adding login and signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import CreateLink from "./components/CreateLink";
+import Header from "./components/Header";
 import LinkList from "./components/LinkList";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      HOME
-      <LinkList />
+    <div className="center w85">
+      <Header />
+
+      <div className="ph3 pv1 background-gray">
+        <Routes>
+          <Route path="/" element={<LinkList />} />
+          <Route path="/create" element={<CreateLink />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,54 @@
+import { NavLink, useNavigate } from "react-router-dom";
+
+import React from "react";
+import { AUTH_TOKEN } from "../constants";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+
+  return (
+    <div className="flex pal justify-between nowrap orange">
+      <div className="flex flex-fixed black">
+        <NavLink to="/" className="no-underline black">
+          <div className="fw7 mr1">Hacker News</div>
+        </NavLink>
+        <NavLink to="/" className="ml1 no-underline black">
+          new
+        </NavLink>
+        <div className="ml1">|</div>
+        <NavLink to="/search" className="ml1 no-underline black">
+          search
+        </NavLink>
+        {authToken && (
+          <div className="flex">
+            <div className="ml1">|</div>
+            <NavLink to="/create" className="ml1 no-underline black">
+              submit
+            </NavLink>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-fixed">
+        {authToken ? (
+          <div
+            className="ml1 pointer black"
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN);
+              navigate(`/`);
+            }}
+          >
+            logout
+          </div>
+        ) : (
+          <NavLink to="/login" className="ml1 no-underline black">
+            login
+          </NavLink>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
