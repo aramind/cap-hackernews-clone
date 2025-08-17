a831e6c5abb096d997d3108bd63421e366ce0c5d
aramind <88814622+aramind@users.noreply.github.com>
2025-08-17 23:26:58 +0800

    feat: adding login and signup action gql

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,14 +1,59 @@
+import { gql, useMutation } from "@apollo/client";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { AUTH_TOKEN } from "../constants";
 
 const Login = () => {
   const navigate = useNavigate();
+
+  const SIGNUP_MUTATION = gql`
+    mutation SigupMutation(
+      $email: String!
+      $password: String!
+      $name: String!
+    ) {
+      signup(email: $email, password: $password, name: $name) {
+        token
+      }
+    }
+  `;
+
+  const LOGIN_MUTATION = gql`
+    mutation LoginMutation($email: String!, $password: String!) {
+      login(email: $email, password: $password) {
+        token
+      }
+    }
+  `;
 
   const [formState, setFormState] = useState({
     login: true,
     email: "",
     password: "",
     name: "",
+  });
+
+  const [login] = useMutation(LOGIN_MUTATION, {
+    variables: {
+      email: formState.email,
+      password: formState.password,
+    },
+    onCompleted: ({ login }) => {
+      localStorage.setItem(AUTH_TOKEN, login.token);
+      navigate("/");
+    },
+  });
+
+  const [signup] = useMutation(SIGNUP_MUTATION, {
+    variables: {
+      name: formState.name,
+      email: formState.email,
+      password: formState.password,
+    },
+    onCompleted: ({ signup }) => {
+      localStorage.setItem(AUTH_TOKEN, signup.token);
+      navigate("/");
+    },
   });
 
   return (
@@ -54,7 +99,7 @@ const Login = () => {
       <div className="flex mt3">
         <button
           className="pointer mr2 button"
-          onClick={() => console.log("onClick")}
+          onClick={formState.login ? login : signup}
         >
           {formState.login ? "login" : "create account"}
         </button>
